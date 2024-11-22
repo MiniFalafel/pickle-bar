77535ec78dce91cc1a74e7e622b86622dc279196
Eli Sanders <69729819+MiniFalafel@users.noreply.github.com>
2024-11-22 16:14:44 -0700

    Launch script no longer relies on specific path

--- launch.sh
+++ launch.sh
@@ -5,4 +5,6 @@ config=$SCRIPT_DIR/bar.json
 style=$SCRIPT_DIR/style.css
 
 killall waybar
+# Our waybar config needs this variable, so we're setting it before calling waybar
+export WB_SCRIPT_PATH=$SCRIPT_DIR/scripts
 waybar -c "$config" -s "$style"
