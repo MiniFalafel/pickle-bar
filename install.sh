b4dfebc00b11092a35268ca5478edd73035553a2
Eli Sanders <69729819+MiniFalafel@users.noreply.github.com>
2024-11-22 20:51:36 -0700

    Updated install script to check for dependency files first

--- install.sh
+++ install.sh
@@ -3,8 +3,12 @@
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 # Install dependencies
-pacman -S - < "$SCRIPT_DIR/pkglist.txt"
+if test -f "$SCRIPT_DIR/pkglist.txt"; then
+    pacman -S - < "$SCRIPT_DIR/pkglist.txt"
+fi
 # Install AUR dependencies as non-root user
-NON_ROOT=$(who am i | awk '{print $1}')
-sudo -u $NON_ROOT yay -S - < "$SCRIPT_DIR/yay-pkglist.txt"
+if test -f "$SCRIPT_DIR/yay-pkglist.txt"; then
+    NON_ROOT=$(who am i | awk '{print $1}')
+    sudo -u $NON_ROOT bash -c "yay -S - < $SCRIPT_DIR/yay-pkglist.txt"
+fi
 
