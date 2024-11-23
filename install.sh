98f77aa8059b50293d19a164129d47d43175a1dc
Eli Sanders <69729819+MiniFalafel@users.noreply.github.com>
2024-11-22 23:18:22 -0700

    Updated install to only install packages that aren't already installed

--- install.sh
+++ install.sh
@@ -4,11 +4,11 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 # Install dependencies
 if test -f "$SCRIPT_DIR/pkglist.txt"; then
-    pacman -S - < "$SCRIPT_DIR/pkglist.txt"
+    pacman -S --needed - < "$SCRIPT_DIR/pkglist.txt"
 fi
 # Install AUR dependencies as non-root user
 if test -f "$SCRIPT_DIR/yay-pkglist.txt"; then
     NON_ROOT=$(who am i | awk '{print $1}')
-    sudo -u $NON_ROOT bash -c "yay -S - < $SCRIPT_DIR/yay-pkglist.txt"
+    sudo -u $NON_ROOT bash -c "yay -S --needed - < $SCRIPT_DIR/yay-pkglist.txt"
 fi
 
